71331eebfa711538bbecc22d0d7301eb692602f7
asimshohrab7 <asimsohrab7@gmail.com>
2025-12-27 10:35:55 +0530

    Ready for Deployment

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,76 @@
+import React, { useContext, useState } from 'react'
+import withAuth from '../utils/withAuth'
+import { useNavigate } from 'react-router-dom'
+import "../App.css";
+import { Button, IconButton, TextField } from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+import { AuthContext } from '../contexts/AuthContext';
+
+function HomeComponent() {
+
+
+    let navigate = useNavigate();
+    const [meetingCode, setMeetingCode] = useState("");
+
+
+    const {addToUserHistory} = useContext(AuthContext);
+    let handleJoinVideoCall = async () => {
+        await addToUserHistory(meetingCode)
+        navigate(`/${meetingCode}`)
+    }
+
+    return (
+        <>
+
+            <div className="navBar">
+
+                <div style={{ display: "flex", alignItems: "center" }}>
+
+                    <h1 style={{ color: "#5200ebff" }}><b>AXIMConnect</b></h1>
+                </div>
+
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <IconButton onClick={
+                        () => {
+                            navigate("/history")
+                        }
+                    }>
+                        <RestoreIcon />
+                    </IconButton>
+                    <p>History</p>
+
+                    <Button onClick={() => {
+                        localStorage.removeItem("token")
+                        navigate("/auth")
+                    }}>
+                        Logout
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+            <div className="meetContainer">
+                <div className="leftPanel">
+                    <div>
+                        <h2>Providing Quality Video Call because the Developer is  <span style={{ color: "#5200ebff" }}><b>Khan Asim</b></span> </h2><br /><br />
+
+                        <div style={{ display: 'flex', gap: "10px" }}>
+
+                            <TextField onChange={e => setMeetingCode(e.target.value)} id="outlined-basic" label="Meeting Code" variant="outlined" />
+                            <Button onClick={handleJoinVideoCall} variant='contained'>Join</Button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='rightPanel'>
+                    <img srcSet='/logo3.png' alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default withAuth(HomeComponent)
